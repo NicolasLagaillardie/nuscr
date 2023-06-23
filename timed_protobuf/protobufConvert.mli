e28990b6d592144d09041b2fd75a9987ccc02482
Nicolas <lagaillardie.nicolas@live.fr>
2023-06-23 18:33:08 +0100

    deleted timed_protobuf/

--- timed_protobuf/protobufConvert.mli
+++
@@ -1,7 +0,0 @@
-val of_gtype : Gtype.t -> Globaltype.GlobalType.t
-
-val string_of_gtype : Gtype.t -> string
-
-val of_ltype : Ltype.t -> Localtype.LocalType.t
-
-val string_of_ltype : Ltype.t -> string
